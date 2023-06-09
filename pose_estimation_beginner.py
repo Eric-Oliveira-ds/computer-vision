ad274b03162f977daaf911e4e74476c450aefae4
Eric <eric.oliveira.pro@gmail.com>
2023-06-09 15:51:14 -0300

    sucess pose estimation example

--- pose_estimation_beginner.py
+++ pose_estimation_beginner.py
@@ -3,19 +3,31 @@ import matplotlib.pyplot as plt
 import mediapipe as mp
 import time
 
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+
+
 # capturar video 
 cap = cv2.VideoCapture('Pose Estimation Videos/video2-25fps.mp4')
 pTime = 0
 while True:
     # ler video
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
 
     # checar frame rate
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (90,70), cv2.FONT_HERSHEY_PLAIN, 8, (255, 0, 0), 8)
+    cv2.putText(img, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
 
     key = cv2.waitKey(1)
