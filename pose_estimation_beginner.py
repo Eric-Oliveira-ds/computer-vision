d4b73e8b803a55a00b0ed6bb2f35ba1d2e2e9cc9
Eric <eric.oliveira.pro@gmail.com>
2023-06-09 15:22:24 -0300

    init pose estimation with cap video example

--- pose_estimation_beginner.py
+++ pose_estimation_beginner.py
@@ -1,11 +1,28 @@
 import cv2
 import matplotlib.pyplot as plt
 import mediapipe as mp
+import time
 
 # capturar video 
-cap = cv2.VideoCapture('Pose Estimation Videos/video1-25fps.mp4')
-
+cap = cv2.VideoCapture('Pose Estimation Videos/video2-25fps.mp4')
+pTime = 0
 while True:
+    # ler video
     success, img = cap.read()
+
+    # checar frame rate
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (90,70), cv2.FONT_HERSHEY_PLAIN, 8, (255, 0, 0), 8)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    key = cv2.waitKey(1)
+
+    # Verifica se a tecla 'q' foi pressionada
+    if key == ord('q'):
+        break
+
+    # Fecha a janela
+cv2.destroyAllWindows()
